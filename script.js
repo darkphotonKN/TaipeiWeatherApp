afaafd74916bba78552dd3f32242249a43fa69af
Kranti Nebhwani <darkphoton20@gmail.com>
2018-06-11 22:26:05 +0800

    Added multiple search options to show various info retrived by API.

--- script.js
+++ script.js
@@ -5,33 +5,69 @@
 const mainForm = document.querySelector('.main-form');
 const selection = document.querySelector('select.input');
 const input = document.querySelector('input.input');
-const dataDelayTime = 200;
+const dataDelayTime = 300; // delay for api to deliver code before displaying it to DOM
+
+const searchForm = document.querySelector('#main-screen .main-form');
+
+let results = ""; // result of weather api data 
+
+// event listener for weather search form submit 
+searchForm.addEventListener('submit', e => {
+    // prevent default form submission page refresh
+    e.preventDefault();
 
 
-const mainScreen = document.getElementById('main-screen');
-let forms = mainScreen.getElementsByTagName("form");
+    // let data load before displaying on DOM
+    const outputArea = document.getElementById('output-wrap');
+    setTimeout(() => outputArea.classList.add('show'), dataDelayTime+200);
 
-for(let i=0; i<forms.length; i++) {
+    // if user has not put specific input value use drop down
+    if(input.value === "") {
+        weatherData(selection.value, "main", "temp");
 
-    forms[i].addEventListener('submit', e => {
-        // prevent form submission
-        e.preventDefault();
+        // display at output of DOM
+        const output = document.getElementById("output");
+        setTimeout(() => output.innerHTML += results+"&#8451", dataDelayTime); 
+    } 
+    // if user has entered city name in input prioritize that
+    else {
+        // clear display first 
+        output.innerHTML = "";
+        
+        // target sort-options div and child input elements
+        const sortOptionSection = document.getElementById("sort-options");
+        const sortOptions = sortOptionSection.getElementsByTagName('input'); 
 
-        // let data load before displaying on DOM
-        const outputArea = document.getElementById('output-wrap');
-        setTimeout(() => outputArea.classList.add('show'), dataDelayTime);
+        // store info on which are checked and what information to display
+        let displayInfo = []; 
 
-        // if user has not put specific input value use drop down
-        if(input.value === "") {
-            weatherData(selection.value, "main", "temp");
-        } 
-        // if user has entered city name in input prioritize that
-        else {
-            weatherData(input.value, "main", "temp");
+        for(let i=0; i<sortOptions.length; i++) {
+            
+            // if search option is checked 
+            if(sortOptions[i].checked) {
+                displayInfo.push(sortOptions[i].value); // add them to displayInfo
+            }
         }
-    });
 
-}
+        // display the information 
+        for(let i=0; i<displayInfo.length; i++) {
+            setTimeout(() => {
+                //console.log(`displaying info for ${displayInfo[i]}`);
+                weatherData(input.value, "main", displayInfo[i]);
+
+                
+                const output = document.getElementById("output");
+                const outputTitle = document.querySelector("#output-wrap .output-title");
+
+                // display at output of DOM
+                output.innerHTML += results+"&#8451 ";
+                outputTitle.innerHTML = `${input.value} Weather Information`;
+
+            }, 200+(i*1000));
+        }
+            
+    }
+});
 
 
 
@@ -39,7 +75,6 @@ for(let i=0; i<forms.length; i++) {
 
 
 // get weather api data 
-let results = ""; // result 
 
 function weatherData(cityName, property, subProperty) {
     
@@ -49,14 +84,7 @@ function weatherData(cityName, property, subProperty) {
     .then(data => { 
         results = data[property][subProperty];
     });                                    
-    
-    // display at output of DOM
-    const output = document.getElementById("output");
-    const outputTitle = document.querySelector("#output-wrap .output-title");
-    setTimeout(() => {
-        output.innerHTML = results+"&#8451";
-        outputTitle.innerHTML = cityName + " Temperature";
-    }, dataDelayTime); 
 
-
+    // return results
+    return results;
 }
